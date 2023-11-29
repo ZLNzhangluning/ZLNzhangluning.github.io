bae6b80e388f69cbb800f23dcf2416ee76e62af2
zhangluning <zhangluning2000@163.com>
2023-11-29 16:20:10 +0800

    auto construct blog

--- bin/autoDeploy.bat
+++ bin/autoDeploy.bat
@@ -5,4 +5,4 @@ git init
 git add -A
 git commit -m "auto construct blog"
 
-git push -f https://github.com/ZLNzhangluning/ZLNzhangluning.github.io.git master:gh-pages
+git push -f https://github.com/ZLNzhangluning/ZLNzhangluning.github.io.git main:gh-pages
